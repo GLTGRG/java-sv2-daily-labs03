7d34c062b7d43df51ac2aacd3396a3c130ae18cf
GLTGRG <galatgergo@gmail.com>
2021-10-31 21:40:27 +0100

    day03 feladat

---
+++ src/main/java/day03/GuessTheNumber.java
@@ -0,0 +1,33 @@
+package day03;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+    private int number;
+    private int tipNumber;
+
+    public static void main(String[] args) {
+        GuessTheNumber guessTheNumber = new GuessTheNumber();
+        Random random = new Random();
+        guessTheNumber.tipNumber = random.nextInt();
+        System.out.println(guessTheNumber.calculate());
+    }
+    
+    public String calculate(){
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++){
+            System.out.println("kérem a tipeket");
+            tipNumber = scanner.nextInt();
+            if (tipNumber > number){
+                System.out.println("A keresett szám kissebb");
+            }else if (tipNumber < number){
+                System.out.println("A keresett szám nagyobb");
+            } else {
+                return "kitaláltad a sázmot";
+            }
+        }
+        return "Most nem nyert";
+    }
+}
